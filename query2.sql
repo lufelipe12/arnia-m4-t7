6cb360e5fb8c14f0de3fe6294d77310e4502c296
lufelipe12 <lufelipe.dias@gmail.com>
2024-09-24 15:22:24 -0300

    feat: added new exercise for today 24/09

---
+++ query2.sql
@@ -0,0 +1,40 @@
+-- Tabela developers.
+
+INSERT INTO "developers" ("name", "skills", "is_active", "experience_years")
+VALUES
+    ('John Smith', 'Java, Python', TRUE, 5),
+    ('Emily Johnson', 'C#, SQL', TRUE, 3),
+    ('Michael Brown', 'JavaScript, HTML', FALSE, 2),
+    ('Sophia Wilson', 'Python, Ruby', TRUE, 4),
+    ('William Davis', 'C++, Java', TRUE, 6),
+    ('Emma Jones', 'PHP, SQL', FALSE, 1),
+    ('Liam Martinez', 'Java, JavaScript', TRUE, 7),
+    ('Olivia Thompson', 'Python, CSS', TRUE, 4),
+    ('Noah Anderson', 'C#, HTML', FALSE, 2),
+    ('Ava Garcia', 'JavaScript, Ruby', TRUE, 5),
+    ('Ethan Martinez', 'Java, SQL', TRUE, 6),
+    ('Isabella Walker', 'Python, HTML', TRUE, 3),
+    ('Mason Taylor', 'C++, JavaScript', FALSE, 1),
+    ('Mia White', 'C#, PHP', TRUE, 4),
+    ('James Hall', 'Python, Java', TRUE, 5),
+    ('Charlotte Lee', 'JavaScript, CSS', FALSE, 2),
+    ('Benjamin Turner', 'C++, Ruby', TRUE, 7),
+    ('Amelia Adams', 'Java, SQL', TRUE, 6),
+    ('Harper Hill', 'Python, HTML', TRUE, 0),
+    ('Lucas Martinez', 'C#, JavaScript', FALSE, 11);
+
+
+-- Tabela projects
+
+INSERT INTO "projects" ("project_name", "start_date")
+VALUES
+    ('E-Commerce Website', '2023-01-15'),
+    ('Mobile App Development', '2023-02-20'),
+    ('Data Analytics Platform', '2023-03-10'),
+    ('CRM System Enhancement', '2023-04-05'),
+    ('Inventory Management App', '2023-05-12'),
+    ('Financial Dashboard', '2023-06-18'),
+    ('Healthcare App', '2023-07-25'),
+    ('E-Learning Platform', '2023-08-10'),
+    ('Social Media Integration', '2023-09-02'),
+    ('AI Chatbot Development', '2023-10-15');
