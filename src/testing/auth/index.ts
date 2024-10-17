b3d2500812da435c71f79c2ed622aa0c438e07e0
lufelipe12 <lufelipe.dias@gmail.com>
2024-10-17 20:23:22 -0300

    test: added tests for users controller

--- src/testing/auth/index.ts
+++ src/testing/auth/index.ts
@@ -1,2 +1,3 @@
+export * from './auth-guard.mock';
 export * from './jwt-service.mock';
 export * from './login.mock';
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import {
+  authGuardMock,
+  createUserMock,
+  currentUserMock,
+  updateUserMock,
+  usersServiceMock,
+} from '../testing';
 
-describe('UsersController', () => {
-  let controller: UsersController;
+describe('Users Controller', () => {
+  let usersController: UsersController;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
-    }).compile();
+      providers: [usersServiceMock],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(authGuardMock)
+      .compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('Should be defined', () => {
+    expect(usersController).toBeDefined();
+  });
+
+  describe('Create', () => {
+    it('Should create a new user', async () => {
+      const result = await usersController.create(createUserMock);
+
+      expect(result).toHaveProperty('id');
+    });
+  });
+
+  describe('Read', () => {
+    it('Should return a list of users', async () => {
+      const result = await usersController.show();
+
+      expect(result.length).toBeGreaterThan(0);
+    });
+
+    it('Should return users profile', async () => {
+      const result = await usersController.profile(currentUserMock);
+
+      expect(result.deletedAt).toBeNull();
+    });
+
+    it('Should return an user by id', async () => {
+      const result = await usersController.findById(1);
+
+      expect(result).toBeDefined();
+    });
+  });
+
+  describe('Update', () => {
+    it('Should update an user by id', async () => {
+      const result = await usersController.updateById(1, updateUserMock);
+
+      expect(result.email).toEqual(updateUserMock.email);
+    });
+  });
+
+  describe('Delete', () => {
+    it('Should deactivate an user', async () => {
+      const result = await usersController.deleteBy(1);
+
+      expect(result).toHaveProperty('response');
+    });
+  });
+
+  describe('Auth guard', () => {
+    it('Shoud validate if guard is applied on route', async () => {
+      const guards = Reflect.getMetadata('__guards__', usersController.profile);
+
+      expect(guards.length).toEqual(1);
+      expect(new guards[0]()).toBeInstanceOf(AuthGuard);
+    });
   });
 });
