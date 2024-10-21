257af50d31536aac9460e49ce387b1cc9b37349f
lufelipe12 <lufelipe.dias@gmail.com>
2024-10-21 20:15:30 -0300

    test: testing e2e.

--- src/testing/auth/auth-guard.mock.ts
+++ src/testing/auth/auth-guard.mock.ts
@@ -1,5 +1,13 @@
-import { CanActivate } from '@nestjs/common';
+import { CanActivate, ExecutionContext } from '@nestjs/common';
 
 export const authGuardMock: CanActivate = {
-  canActivate: jest.fn(),
+  canActivate: jest.fn((context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+
+    request['user'] = {
+      userId: 1,
+    };
+
+    return true;
+  }),
 };
