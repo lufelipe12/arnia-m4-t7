be1b4b085106b8f4fda17eb94ba840c8a79533e5
lufelipe12 <lufelipe.dias@gmail.com>
2024-10-23 20:18:06 -0300

    chore: added first configurations for bootstrap

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = new ConfigService();
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('Arnia college.')
+    .setDescription('API made for Arnia students')
+    .setVersion('0.1')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, docConfig);
+  SwaggerModule.setup('v1/docs', app, documentFactory);
+
+  app.setGlobalPrefix('v1/');
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(configService.get('PORT') || 3000);
+}
+bootstrap();
